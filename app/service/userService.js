821a64f96bbe2a530e8e44ba9adb83f4fa5fcede
xuhongyang <836237106@qq.com>
2021-11-02 14:50:22 +0800

    第二次配置

---
+++ app/service/userService.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class UserService extends Service {
+  async login(params) {
+    const { app } = this
+
+  }
+}
+
+module.exports = UserService;

3f4bdaed571c177166a6b7b62a7056b4c5578f20
xuhongyang <836237106@qq.com>
2021-12-14 17:55:16 +0800

    socekt通信修改

--- app/io/controller/sensor.js
+++ app/io/controller/sensor.js
@@ -15,28 +15,6 @@ class SensorController extends Controller {
       nsp.emit('sensor', { errorMsg: err });
     }
   }
-  async count() {
-    const { ctx, app, service } = this;
-    const data = ctx.args[0] || {};
-    const nsp = app.io.of('/console')
-    try {
-      let payload = {
-        nsp,
-        token: ctx.socket.handshake.query.token,
-        socket: ctx.socket,
-        helper: ctx.helper,
-        action: data.action,
-      };
-      const res = await service.core.recordService.count(payload);
-      if (res) {
-        nsp.emit(ctx.socket.id, ctx.helper.socketMsg('task_count', { res, message: 'success' }, { client: ctx.socket.id }))
-      } else {
-        nsp.emit(ctx.socket.id, ctx.helper.socketMsg('task_count', { message: 'fail' }, { client: ctx.socket.id }))
-      }
-    } catch (err) {
-      nsp.emit(ctx.socket.id, ctx.helper.socketMsg('task_count', { message: err }, { client: ctx.socket.id }));
-    }
-  }
 }
 
 module.exports = SensorController

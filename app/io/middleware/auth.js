14e94f626eee32080e1e4151a5fe68756243170a
xuhongyang <836237106@qq.com>
2021-12-14 18:06:10 +0800

    socket连接修改

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -9,7 +9,9 @@ module.exports = () => {
     } else {
       try {
         const decrypt = app.jwt.verify(query.token, app.config.jwt.secret);
-        if (decrypt.id !== undefined && decrypt.id !== null && decrypt.id.length === 19) { // 验证token
+        const database = await app.Helper.prototype.database(app)
+        const user = await database.user.get({ id: decrypt.id })
+        if (user != null) { // 验证token
           socket.conn.on('packet', packet => {
             if (packet.type === 'ping') {
               console.log('success')
